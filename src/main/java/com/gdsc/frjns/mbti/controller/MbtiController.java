f4d57bd3c26618f2f2403b0a35985d9d00a5798a
kikingki <mongbu54@gmail.com>
2023-05-12 16:10:51 +0900

    docs: api 문서화

--- src/main/java/com/gdsc/frjns/mbti/controller/MbtiController.java
+++ src/main/java/com/gdsc/frjns/mbti/controller/MbtiController.java
@@ -2,6 +2,11 @@ package com.gdsc.frjns.mbti.controller;
 
 import com.gdsc.frjns.mbti.dto.MbtiResponse;
 import com.gdsc.frjns.mbti.service.MbtiService;
+import com.gdsc.frjns.news.dto.NewsDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -20,6 +26,14 @@ public class MbtiController {
     private final MbtiService mbtiService;
 
     @GetMapping("/mbti/{sourceMbti}")
+    @Operation(
+            summary = "mbti 궁합 조회",
+            description = "뉴진스 멤버와 나의 MBTI의 궁합을 조회할 수 있습니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청 성공", content = @Content(schema = @Schema(implementation = MbtiResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "존재하지 않는 mbti 요청", content = @Content(schema = @Schema(implementation = ResponseEntity.class)))
+            }
+    )
     public ResponseEntity<List<MbtiResponse>> findAllByMbti(@PathVariable("sourceMbti") String mbti){
         List<MbtiResponse> responses = mbtiService.findAllBySourceMbti(mbti.toUpperCase());
 
--- src/main/java/com/gdsc/frjns/mbti/dto/MbtiResponse.java
+++ src/main/java/com/gdsc/frjns/mbti/dto/MbtiResponse.java
@@ -1,12 +1,18 @@
 package com.gdsc.frjns.mbti.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
+@Schema(description = "mbti 궁합 응답")
 @Builder
 public record MbtiResponse (
+        @Schema(description = "요청 mbti", example = "INTP")
         String sourceMbti,
+        @Schema(description = "결과 이름", example = "민지(ESTJ)")
         String targetName,
+        @Schema(description = "이미지", example = "https://a.img.com/test.png")
         String imageUrl,
+        @Schema(description = "결과 내용", example = "관리자 스타일이며 리더에 적합한 민지와 궁합이 좋군요!")
         String content
 ) {
 }
