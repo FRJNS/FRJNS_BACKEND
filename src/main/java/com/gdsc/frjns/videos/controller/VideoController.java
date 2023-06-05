4b1a26a5131afd0e662e3db308666df02199b5c1
kikingki <mongbu54@gmail.com>
2023-06-06 04:43:24 +0900

    docs: swagger 코드 추가

--- src/main/java/com/gdsc/frjns/videos/controller/VideoController.java
+++ src/main/java/com/gdsc/frjns/videos/controller/VideoController.java
@@ -1,8 +1,13 @@
 package com.gdsc.frjns.videos.controller;
 
+import com.gdsc.frjns.news.dto.NewsDTO;
 import com.gdsc.frjns.videos.domain.model.Video;
 import com.gdsc.frjns.videos.dto.VideoResponse;
 import com.gdsc.frjns.videos.service.VideoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -22,6 +27,13 @@ public class VideoController {
     private final VideoService videoService;
 
     @GetMapping("/search")
+    @Operation(
+            summary = "video 데이터 추가",
+            description = "'뉴진스 세로직캠' 키워드 검색에 대한 새로운 video 데이터를 추가합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청 성공")
+            }
+    )
     public ResponseEntity<String> searchVideos() throws GeneralSecurityException, IOException {
         videoService.saveVideos();
 
@@ -29,6 +41,13 @@ public class VideoController {
     }
 
     @GetMapping("/videos")
+    @Operation(
+            summary = "유튜브 URL 요청",
+            description = "뉴진스 동영상에 대한 유튜브 URL 목록을 응답받을 수 있습니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청 성공", content = @Content(schema = @Schema(implementation = VideoResponse.class)))
+            }
+    )
     public ResponseEntity<Slice<VideoResponse>> findAll(@PageableDefault(size = 5) Pageable pageable) {
         Slice<VideoResponse> videoResponses = videoService.findAll(pageable);
 
--- src/main/java/com/gdsc/frjns/videos/dto/VideoResponse.java
+++ src/main/java/com/gdsc/frjns/videos/dto/VideoResponse.java
@@ -1,11 +1,16 @@
 package com.gdsc.frjns.videos.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
+@Schema(description = "mbti 궁합 응답")
 @Builder
 public record VideoResponse(
+        @Schema(description = "video id", example = "12")
         Long id,
+        @Schema(description = "video 제목", example = "[릴레이댄스] NewJeans(뉴진스) - Hype Boy (4K)")
         String title,
+        @Schema(description = "video url", example = "https://www.youtube.com/watch?v=NcJo-T5Wo3U")
         String url
 ) {
 }
